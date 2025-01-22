9c172467278cc0a01cb1dc3bf4e82a5f3ed3f5e0
meteor <liuz46360@gmail.com>
2025-01-22 22:11:29 +0800

    init version for frontend

---
+++ test_game_frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GameMap from './components/GameMap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Game Map Viewer</h1>
+      <GameMap />
+    </div>
+  );
+}
+
+export default App; 
